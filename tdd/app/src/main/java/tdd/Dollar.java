0a2331ce4f3e0d235da9451f5af835d8c791ca03
hhashimoto <almite.bridge@gmail.com>
2021-03-14 04:05:16 +0900

    Green: amountの設定をtimesに移動

--- tdd/app/src/main/java/tdd/Dollar.java
+++ tdd/app/src/main/java/tdd/Dollar.java
@@ -1,11 +1,12 @@
 package tdd;
 
 public class Dollar {
-    int amount = 10;
+    int amount;
 
     Dollar(int amount) {
     }
 
     void times(int multiplier) {
+        this.amount = 5 * 2;
     }
 }
