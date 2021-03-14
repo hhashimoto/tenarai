40827c3311188978015633b9fe3efb7b655ba604
hhashimoto <almite.bridge@gmail.com>
2021-03-14 13:23:29 +0900

    Green: Franc(フラン)の追加

---
+++ tdd/app/src/main/java/tdd/Franc.java
@@ -0,0 +1,18 @@
+package tdd;
+
+public class Franc {
+    private int amount;
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(this.amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc dollar = (Franc) object;
+        return this.amount == dollar.amount;
+    }
+}
