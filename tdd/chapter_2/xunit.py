fed2cd0ffe9a8cc6580f6a1677fe4aa39a5223bb
hhashimoto <almite.bridge@gmail.com>
2021-03-21 01:53:39 +0900

    Green: テストメソッドを呼び出す基底クラスTestCaseと、テストを実装する子クラスに分離した

--- tdd/chapter_2/xunit.py
+++ tdd/chapter_2/xunit.py
@@ -1,15 +1,19 @@
 #%%
-class WasRun:
+class TestCase:
     def __init__(self, name):
-        self.wasRun = None
         self.name = name
-
-    def testMethod(self):
-        self.wasRun = 1
 
     def run(self):
         method = getattr(self, self.name)
         method()
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        super().__init__(name)
+
+    def testMethod(self):
+        self.wasRun = 1
 
 test = WasRun('testMethod')
 print(test.wasRun)
