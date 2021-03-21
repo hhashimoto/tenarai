735821d4ff2e2f925f1d3c256497967460064756
hhashimoto <almite.bridge@gmail.com>
2021-03-21 09:56:16 +0900

    Green: メソッド実行順を記録できるように変更し、testSetUpではログを比較するようにした

--- tdd/chapter_2/xunit.py
+++ tdd/chapter_2/xunit.py
@@ -18,6 +18,7 @@ class WasRun(TestCase):
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = 1
+        self.log = "setUp "
 
 class TestCaseTest(TestCase):
     def setUp(self):
@@ -29,7 +30,7 @@ class TestCaseTest(TestCase):
 
     def testSetUp(self):
         self.test.run()
-        assert(self.test.wasSetUp)
+        assert('setUp ' == self.test.log)
 
 TestCaseTest('testRunning').run()
 TestCaseTest('testSetUp').run()
