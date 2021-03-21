077688fb17886e850db67255bd0ba01d50ae8513
hhashimoto <almite.bridge@gmail.com>
2021-03-21 10:05:59 +0900

    Green: tearDownを実装しテスト対象のあとで呼ばれるようにした

--- tdd/chapter_2/xunit.py
+++ tdd/chapter_2/xunit.py
@@ -7,22 +7,29 @@ class TestCase:
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
 class WasRun(TestCase):
     def testMethod(self):
-        self.log = self.log + "testMethod "
+        self.log = self.log + 'testMethod '
 
     def setUp(self):
-        self.log = "setUp "
+        self.log = 'setUp '
+
+    def tearDown(self):
+        self.log = self.log + 'tearDown '
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun('testMethod')
         test.run()
-        assert('setUp testMethod tearDown' == test.log)
+        assert('setUp testMethod tearDown ' == test.log)
 
 TestCaseTest('testTemplateMethod').run()
 
