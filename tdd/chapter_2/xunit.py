e4197455b149404d9e60c9414288c77fb426cec4
hhashimoto <almite.bridge@gmail.com>
2021-03-21 09:48:28 +0900

    Green: setUpメソッドを追加し、テスト実行時に呼び出すようにした

--- tdd/chapter_2/xunit.py
+++ tdd/chapter_2/xunit.py
@@ -4,8 +4,12 @@ class TestCase:
         self.name = name
 
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+
+    def setUp(self):
+        pass
 
 class WasRun(TestCase):
     def __init__(self, name):
@@ -14,6 +18,9 @@ class WasRun(TestCase):
 
     def testMethod(self):
         self.wasRun = 1
+
+    def setUp(self):
+        self.wasSetUp = 1
 
 class TestCaseTest(TestCase):
     def testRunning(self):
