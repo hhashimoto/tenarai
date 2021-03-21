8f09f3ec3ce37e6c1242f5f0c82495698c9032cf
hhashimoto <almite.bridge@gmail.com>
2021-03-21 10:15:39 +0900

    Green: runがTestResultインスタンスを返すようにした

--- tdd/chapter_2/xunit.py
+++ tdd/chapter_2/xunit.py
@@ -1,4 +1,8 @@
 #%%
+class TestResult:
+    def summary(self):
+        return '1 run, 0 failed'
+
 class TestCase:
     def __init__(self, name):
         self.name = name
@@ -8,6 +12,7 @@ class TestCase:
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return TestResult()
 
     def setUp(self):
         pass
