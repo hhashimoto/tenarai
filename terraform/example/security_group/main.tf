0904c12070a39d29925c52f923259e390c131566
hhashimoto <almite.bridge@gmail.com>
2021-04-05 03:44:39 +0900

    [Terraform] セキュリティグループのアウトバウンド設定ミスを修正

--- terraform/example/security_group/main.tf
+++ terraform/example/security_group/main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group_rule" "egress" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = var.cidr_blocks
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.default.id
 }
 
