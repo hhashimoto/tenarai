1e9f3aa59872dd6133e27ae78e541d92b3eb4020
hhashimoto <almite.bridge@gmail.com>
2021-04-04 18:35:52 +0900

    [Terraform] ルートテーブルの定義
    - インターネットとの通信のために、ルーティング情報が必要
    - ルーティング情報を管理するのがルートテーブル
    - ルートテーブルはVPCに紐づく
    - ルートはルートテーブルの1レコード
    - ルートテーブルの使用はサブネット単位、サブネットに対して紐付ける

--- terraform/example/network.tf
+++ terraform/example/network.tf
@@ -26,3 +26,18 @@ resource "aws_internet_gateway" "example" {
     Name = "example"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
