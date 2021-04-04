6b8559279ce4c879abddf629fd5321ab5ca99351
hhashimoto <almite.bridge@gmail.com>
2021-04-04 17:10:15 +0900

    [Terraform] IAMロールモジュールの実装

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -19,3 +19,18 @@ module "web_server" {
 variable "example_instance_type" {
   default = "t3.micro"
 }
+
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:DescribeRegions"] # リージョン一覧を取得する
+    resources = ["*"]
+  }
+}
