d8a1fe7cde2f03609704e4028489077eb4394a73
hhashimoto <almite.bridge@gmail.com>
2021-04-04 22:33:03 +0900

    [Terraform] プライベートサブネットからインターネットへのルーティング設定
    パブリックサブネットと異なり、設定するゲートウェイはNATGateway。
    gateway_idではなく、nat_gateway_idを設定するので、注意が必要。

--- terraform/example/network.tf
+++ terraform/example/network.tf
@@ -80,3 +80,9 @@ resource "aws_nat_gateway" "example" {
     Name = "example"
   }
 }
+
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  nat_gateway_id         = aws_nat_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
