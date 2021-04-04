18ee368b4ca08ad8bb265a45311dbda8698c6a92
hhashimoto <almite.bridge@gmail.com>
2021-04-04 15:19:05 +0900

    [Terraform] EC2インスタンスの定義
    - variable
      - 変数定義。外から何も流し込まれなかった場合、defaultが使われる。
      - コマンドライン引数、または環境変数で実行時に上書きができる。
      - terraform plan -var 'example_instance_type=t3.nano'
      - TF_VAR_example_instance_type=t3.nano terraform plan
    - output
      - モジュールからの出力を定義。モジュール外から、定義した名前で参照可能
    - data
      - データソース。外部データ参照が可能。
      - AMIのうち、amazonがオーナー、有効、名前が該当する、最新のものを取得

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -10,3 +10,31 @@ terraform {
 provider "aws" {
   region = "ap-northeast-1"
 }
+
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type = var.example_instance_type
+}
+
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+output "example_instance_id" {
+  value = aws_instance.example.id
+}
