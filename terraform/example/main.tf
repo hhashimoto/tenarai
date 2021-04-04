e5e9b971e5e3f3cd59c26a1b941fbbe1606f0e51
hhashimoto <almite.bridge@gmail.com>
2021-04-04 17:41:59 +0900

    [Terraform] パブリックバケットの設定
    - acl: アクセス権の設定。デフォルトはprivate
    - cors_rule: CORSの設定

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -58,3 +58,15 @@ resource "aws_s3_bucket_public_access_block" "private" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket" "public" {
+  bucket = "h2-public-pragmatic-terraform"
+  acl    = "public-read"
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
